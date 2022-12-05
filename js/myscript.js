7ea0579d65c11efda38517fce9c9791821809242
Jake Lockley <119286643+Jakelockley0@users.noreply.github.com>
2022-12-05 12:19:17 +0530

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+    function openForm(){
+  document.getElementById("login-page").style.display="block";
+  }
+  function closeForm(){
+      document.getElementById("login-page").style.display="none";
+  }
+function openReg(){
+  document.getElementById("reg-page").style.display="block";
+}
+function closeSignup(){
+  document.getElementById("reg-page").style.display="none";
+}
+function openMenu(){
+  document.getElementById("menu").style.position="absolute";
+  document.getElementById("menu").style.right="-.5rem";
+  // document.getElementById("menu-btn").style.color="white";
+}
+function closeMenu(){
+  document.getElementById("menu").style.position="absolute";
+  document.getElementById("menu").style.right="-40rem";
+ }
